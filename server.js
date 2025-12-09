8df50b743741a3ef88b7287905a84b00323122f9
Sirjan Tamang <56628702+sirjandrn25@users.noreply.github.com>
2025-12-09 10:40:31 +0545

    enable cors

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import cors from "cors";
 
 const app = express();
+app.use(cors({
+  origin:"http://localhost:3000"
+}));
 app.use(express.json());
 
 // ---------------------------
