8419ed5cdc7c2dad1bb757a4921ee6e2a73b8070
Sirjan Tamang <56628702+sirjandrn25@users.noreply.github.com>
2026-01-27 11:46:54 +0545

    Refactor server to use port 4000

--- server.js
+++ server.js
@@ -280,4 +280,5 @@ app.post("/invoices", authenticateToken, (req, res) => {
   });
 });
 // ---------------------------
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(4000, () => console.log("Server running on port 4000"));
+
